d341be1310dd83accfafb9bccd64216a8ae0b098
cpon00 <cpon712@gmail.com>
2021-03-12 15:28:44 -0800

    pushing for visibility

--- src/parser.js
+++ src/parser.js
@@ -16,6 +16,7 @@ const midiChlorianGrammar = ohm.grammar(String.raw`Midichlorian {
               | execute Exp?                       --return
               | Class
     Command   = lifeform id "=" Exp
+    Designation = id "=" Exp
     Params    = "(" ListOf<Param, ","> ")"
     Param     = lifeform id
     Call      = id "(" Args ")"
@@ -56,9 +57,7 @@ const midiChlorianGrammar = ohm.grammar(String.raw`Midichlorian {
               | transmission+
               | "(" Exp ")"                         --parens
               | "[" ListOf<Exp, ","> "]"            --array
-              | "{" ListOf<Exp, ","> "}"            --dict
-    Designation       = Exp
-              | Exp
+              | "{" ListOf<Exp, ":"> "}"            --dict
     Arg      = id ":" Exp
     relop     = "<=" | "<" | "oneWith" | "!oneWith" | ">=" | ">"
     cred       = digit+
@@ -87,14 +86,157 @@ const midiChlorianGrammar = ohm.grammar(String.raw`Midichlorian {
 //add some interesting feature beacause we are dynamically typed
 //functions as types
 
+const astBuilder = carlosGrammar.createSemantics().addOperation('ast', {
+    Program(body) {
+        return new ast.Program(body.ast())
+    },
+    Command(kind, id, _eq, initializer) {
+        const [name, readOnly] = [id.sourceString, kind.sourceString == 'const']
+        return new ast.VariableDeclaration(name, readOnly, initializer.ast())
+    },
+    FunDecl(_fun, id, parameters, _colons, returnType, body) {
+        const returnTypeTree = returnType.ast()
+        return new ast.FunctionDeclaration(
+            id.sourceString,
+            parameters.ast(),
+            returnTypeTree.length === 0 ? null : returnTypeTree[0],
+            body.ast()
+        )
+    },
+    Params(_left, bindings, _right) {
+        return bindings.asIteration().ast()
+    },
+    Param(id, _colon, type) {
+        return new ast.Parameter(id.sourceString, type.ast())
+    },
+    TypeExp_array(_left, baseType, _right) {
+        return new ast.ArrayType(baseType.ast())
+    },
+    TypeExp_function(inputType, _arrow, outputType) {
+        return new ast.FunctionType(inputType.ast(), outputType.ast())
+    },
+    TypeExp_named(id) {
+        return new ast.TypeName(id.sourceString)
+    },
+    TypeExps(_left, memberTypeList, _right) {
+        return memberTypeList.asIteration().ast()
+    },
+    Statement_assign(variable, _eq, expression) {
+        return new ast.Assignment(variable.ast(), expression.ast())
+    },
+    Statement_print(_print, expression) {
+        return new ast.PrintStatement(expression.ast())
+    },
+    WhileStmt(_while, test, body) {
+        return new ast.WhileStatement(test.ast(), body.ast())
+    },
+    IfStmt_long(_if, test, consequent, _else, alternative) {
+        return new ast.IfStatement(
+            test.ast(),
+            consequent.ast(),
+            alternative.ast()
+        )
+    },
+    unleash(_) {
+        return new ast.BreakStatement()
+    },
+    endure(_) {
+        return new ast.ContinueStatement()
+    },
+    execute(_return, expression) {
+        const returnValueTree = expression.ast()
+        if (returnValueTree.length === 0) {
+            return new ast.ShortReturnStatement()
+        }
+        return new ast.ReturnStatement(returnValueTree[0])
+    },
+    Body(_open, body, _close) {
+        // This one is fun, don't wrap the statements, just return the list
+        return body.ast()
+    },
+    Exp_or(first, _ors, rest) {
+        return new ast.OrExpression([first.ast(), ...rest.ast()])
+    },
+    Exp_and(first, _ors, rest) {
+        return new ast.AndExpression([first.ast(), ...rest.ast()])
+    },
+    Exp1_binary(left, op, right) {
+        return new ast.BinaryExpression(
+            op.sourceString,
+            left.ast(),
+            right.ast()
+        )
+    },
+    Exp2_binary(left, op, right) {
+        return new ast.BinaryExpression(
+            op.sourceString,
+            left.ast(),
+            right.ast()
+        )
+    },
+    Exp3_binary(left, op, right) {
+        return new ast.BinaryExpression(
+            op.sourceString,
+            left.ast(),
+            right.ast()
+        )
+    },
+    Exp4_binary(left, op, right) {
+        return new ast.BinaryExpression(
+            op.sourceString,
+            left.ast(),
+            right.ast()
+        )
+    },
+    Exp4_unary(op, operand) {
+        return new ast.UnaryExpression(op.sourceString, operand.ast())
+    },
+    Exp6_call(callee, _left, args, _right) {
+        return new ast.Call(callee.ast(), args.ast())
+    },
+    Exp6_subscript(array, _left, subscript, _right) {
+        return new ast.SubscriptExpression(array.ast(), subscript.ast())
+    },
+    Exp6_id(id) {
+        return new ast.IdentifierExpression(id.sourceString)
+    },
+    Exp7_array(arrayType, _left, args, _right) {
+        return new ast.ArrayLiteral(arrayType.ast(), args.ast())
+    },
+    Exp7_parens(_open, expression, _close) {
+        return expression.ast()
+    },
+    Args(expressions) {
+        return expressions.asIteration().ast()
+    },
+    true(_) {
+        return true
+    },
+    false(_) {
+        return false
+    },
+    num(_whole, _point, _fraction, _e, _sign, _exponent) {
+        return Number(this.sourceString)
+    },
+    stringlit(_open, chars, _close) {
+        return chars.sourceString
+    },
+})
+
 const astBuilder = midiChlorianGrammar.createSemantics().addOperation('ast', {
     // Program(body) {
     //     return new ast.Program(body.ast())
     // },
-    // Designation(type) {
-    //     return new ast.Designation(type.ast())
+    // Directive(body) {
+    //     return new ast.Directive(body.ast())
     // },
-    //Command(type, id, body) {
+    // Command(left, op, right) {
+    //     return new ast.Command(op.sourceString, left.ast(), right.ast())
+    // }
+    // Designation(left, op, right) {
+    //     return new ast.Designation(op.sourceString, left.ast(), right.ast())
+    // },
+    // Command(type, id, body) {
     //     return new ast.Command(type.ast(), id.sourceString, body.ast())
     // },
     //  Exp1_binary(left, op, right) {
--- test/parser.test.js
+++ test/parser.test.js
@@ -29,32 +29,13 @@ const goodPrograms = [
     `transmission x = "the force"`,
     `transmission message = "Help me Obi-Wan, you're my only hope"`,
     `ket y = 2.0`,
+    `squadron red {
+        imprint (cred x, transmission y) {
+            x = x
+            y = y
+        }
+    }`,
 ]
-
-// const starWarsLines = [
-//     `"'Help me, Obi-Wan Kenobi. You’re my only hope.' — Leia Organa"`,
-//     `"'I find your lack of faith disturbing.' — Darth Vader"`,
-//     `"'It's the ship that made the Kessel run in less than twelve parsecs. I’ve outrun Imperial starships. Not the local bulk cruisers, mind you. I’m talking about the big Corellian ships, now. She’s fast enough for you, old man.' — Han Solo"`,
-//     `"'The Force will be with you. Always.' — Obi-Wan Kenobi"`,
-//     `"'Never tell me the odds!' — Han Solo"`,
-//     `"'Fear is the path to the dark side. Fear leads to anger; anger leads to hate; hate leads to suffering. I sense much fear in you.' — Yoda"`,
-//     `"'I’m just a simple man trying to make my way in the universe.' — Jango Fett"`,
-//     `"'So this is how liberty dies. With thunderous applause.' — Padmé Amidala"`,
-//     `""`,
-// ]
-
-// const droidCommunications = [
-//     0x4a656469205363756d,
-//     0x4c6f6f6b212041204a65646921,
-//     0x4669726521,
-//     0x5765206e656564206d6f7265206261636b757021,
-//     0x526f67657220726f676572,
-//     0x47454e4552414c204b454e4f4249,
-//     12345678910,
-//     '\\c',
-//     66,
-//     27000,
-// ]
 
 const badPrograms = [`let y = 1`, `pogguers y = true`]
 
